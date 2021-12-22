67407a15fe8527ac0d591ef05cc3be58cee1c739
Hodym <hodimchukdmitriy@gmail.com>
2021-12-22 14:22:58 +0200

    connect styles

--- themes/auto/footer.php
+++ themes/auto/footer.php
@@ -27,12 +27,8 @@
     </div>
   </footer>
 
+<?php wp_footer(); ?>
 
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="js/slick.min.js"></script>
-  <script src="js/jquery.fancybox.min.js"></script>
-  <script src="js/wow.min.js"></script>
-  <script src="js/main.js"></script>
 </body>
 
 </html>
